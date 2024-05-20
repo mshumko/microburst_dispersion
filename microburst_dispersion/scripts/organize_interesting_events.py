751e9b3cbdca0b3fc3d4afffdc6aca2c3bfc24cc
Mike Shumko <mike.shumko@jhuapl.edu>
2024-05-20 14:37:06 -0400

    Cleaned up the code.

--- microburst_dispersion/scripts/organize_interesting_events.py
+++ microburst_dispersion/scripts/organize_interesting_events.py
@@ -7,14 +7,15 @@ import pandas as pd
 
 import microburst_dispersion
 
+base_dir = microburst_dispersion.config['here'].parent
+
 interesting_microbust_list = pd.read_excel(
-    'C:\\Users\\shumkms1\\Documents\\research\\microburst_dispersion\\data\\'
-    'firebird_interesting_dispersion_examples.xlsx',
+    base_dir / 'data' / 'firebird_interesting_dispersion_examples.xlsx',
     skiprows=1, 
     usecols='A:C'  # We don't need any of the other columns for now.
     )
 
-input_plot_dir = save_dir = microburst_dispersion.config['here'].parent / 'plots' / 'dispersion_summary'
+input_plot_dir = base_dir / 'plots' / 'dispersion_summary'
 output_plot_dir = input_plot_dir / 'interesting'
 
 if not pathlib.Path(output_plot_dir).exists():
@@ -22,12 +23,9 @@ if not pathlib.Path(output_plot_dir).exists():
 
 for i, row in interesting_microbust_list.iterrows():
     input_plot_path = input_plot_dir / (row['plot_filename'] + '.png')
-
-    # This is how you copy using pathlib.
     output_name = (row['plot_filename'] + '.png')
     output_name_chunks = output_name.split('_')
     output_name_chunks[3] = f'{row["dispersion"]}_{output_name_chunks[3]}'
     output_plot_path = output_plot_dir / '_'.join(output_name_chunks)
+    # This is how you copy using pathlib.
     output_plot_path.write_bytes(input_plot_path.read_bytes())
-
-pass
